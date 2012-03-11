898734c9b57069949ab3f4a7bd17bf9f142c1472
unknown <calvin@x60.(none)>
2012-03-11 22:11:20 +0800

    add phoronix for mobile

---
+++ phoronix/load.js
@@ -0,0 +1,55 @@
+
+function doAjax(url, insertto, selector, remove, callback){
+	var container = $(insertto);
+	if(url.match('^http')){
+		$.getJSON("http://query.yahooapis.com/v1/public/yql?"+
+				"q=select%20*%20from%20html%20where%20url%3D%22"+
+				encodeURIComponent(url)+
+				"%22&format=xml'&callback=?",
+				function(data){
+					if(data.results[0]){
+						var data = data.results[0];
+						var tmp = $(data);
+						var a = tmp.find(selector);
+						a.find(remove).remove();
+						container.html(a);
+						callback();
+					} else {
+						var errormsg = '<p>Error: could not load the page.</p>';
+						container.html(errormsg);
+					}
+				}
+			 );
+	} else {
+		$('#target').load(url);
+	}
+}
+
+
+function onload(){
+
+	//load is not working due to cross domain
+	//$('#phoronix').load('http://www.phoronix.com/scan.php?page=home #phxcms_content_phx', function() {
+	
+	doAjax('http://www.phoronix.com/scan.php?page=home', 
+			'#phoronix', '#phxcms_content_phx', '.phxcms_contentphx_right_bar', function() {
+
+				var a = $('#phxcms_content_phx');
+				a.children().removeAttr('style');
+
+				$('a').click(function(event){
+					event.preventDefault(); 
+					console.log($(this).attr('href'));
+					$('#phoronix').hide();
+					doAjax('http://www.phoronix.com/' + $(this).attr('href'), 
+						'#story', '.KonaBody', '' , function() {
+							$('#story').show();
+						});
+				});
+			});
+
+	$('#summary').click(function(){
+		$('#story').hide();
+		$('#phoronix').show();
+	});
+}
